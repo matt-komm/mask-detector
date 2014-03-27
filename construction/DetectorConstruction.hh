8554f762edb93fd21c8ca418b0c682e6f24636f8
matt-komm <Matthias.Komm@cern.ch>
2014-03-27 12:31:46 +0100

    initial commit

---
+++ construction/DetectorConstruction.hh
@@ -0,0 +1,38 @@
+
+
+#ifndef DetectorConstruction_H
+#define DetectorConstruction_H 1
+
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+
+#include "G4VUserDetectorConstruction.hh"
+
+class DetectorConstruction : public G4VUserDetectorConstruction
+{
+  public:
+
+    DetectorConstruction();
+    ~DetectorConstruction();
+
+    G4VPhysicalVolume* Construct();
+
+  private:
+    
+    // Logical volumes
+    //
+    G4LogicalVolume* experimentalHall_log;
+    G4LogicalVolume* tracker_log;
+    G4LogicalVolume* calorimeterBlock_log;
+    G4LogicalVolume* calorimeterLayer_log;
+
+    // Physical volumes
+    //
+    G4VPhysicalVolume* experimentalHall_phys;
+    G4VPhysicalVolume* calorimeterLayer_phys;
+    G4VPhysicalVolume* calorimeterBlock_phys;
+    G4VPhysicalVolume* tracker_phys;
+};
+
+#endif
+
